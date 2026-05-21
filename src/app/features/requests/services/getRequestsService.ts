43a94cf1dc73045ed9d1865bfe2ea354e6c028d8
ChaoticLucifer <161860485+LeoBarsolaDev@users.noreply.github.com>
2026-05-21 12:01:48 -0300

    requests fix

--- src/app/features/requests/services/getRequestsService.ts
+++ src/app/features/requests/services/getRequestsService.ts
@@ -1,4 +1,4 @@
 import { api } from "../../../shared/services/apiClient";
 import type { RequestResponse } from "../types/requestsType";
 
-export const read_requests = () => api.get<RequestResponse>('/rrhh/requests');
+export const read_requests = () => api.get<RequestResponse>('/procedures/requests/rrhh');
